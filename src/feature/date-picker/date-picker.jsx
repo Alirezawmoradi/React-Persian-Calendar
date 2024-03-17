7214ab3523faec5a3846f4a59aad536c59345a33
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-17 22:53:29 +0330

    dropdown component added to date picker component

--- src/feature/date-picker/date-picker.jsx
+++ src/feature/date-picker/date-picker.jsx
@@ -1,8 +1,12 @@
+import {Dropdown} from "../../components/dropdown/dropdown.jsx";
+
 export const DatePicker = () => {
     return (
         <div className='flex flex-col min-h-screen uppercase'>
             <div className='flex-1 flex  justify-center items-center'>
-                <div className='flex justify-center items-center w-full font-bold'> Custom Date Picker</div>
+                <div className='flex justify-center items-center w-full font-bold'>
+                    <Dropdown/>
+                </div>
             </div>
         </div>
     )
