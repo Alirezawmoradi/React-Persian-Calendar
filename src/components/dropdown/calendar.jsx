0e1cef6388ee6b29a86e0a8d168bfcc7a422ce58
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-17 22:54:01 +0330

    calendar component created

---
+++ src/components/dropdown/calendar.jsx
@@ -0,0 +1,9 @@
+export const Calendar = () => {
+    return (
+        <div className='flex flex-col justify-center w-screen h-auto px-7'>
+            <div className='rounded-lg border bg-gray-200'>
+                Show Calendar
+            </div>
+        </div>
+    )
+}
