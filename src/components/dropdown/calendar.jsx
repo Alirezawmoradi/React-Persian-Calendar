440eee6fdca48ce60338b148caa0223b153d95f0
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-18 18:51:48 +0330

    working on generating dates

--- src/components/dropdown/calendar.jsx
+++ src/components/dropdown/calendar.jsx
@@ -1,4 +1,7 @@
+import {GenerateDate} from "../../utils/generate-date.js";
+
 export const Calendar = () => {
+    console.log(GenerateDate())
     return (
         <div className='flex flex-col justify-center w-screen h-auto px-7'>
             <div className='rounded-lg border bg-gray-200'>
