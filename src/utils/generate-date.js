66c028e1e4eb78164d8e11e0c8d9f402484ba723
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-19 23:58:08 +0330

    generate date file completed

--- src/utils/generate-date.js
+++ src/utils/generate-date.js
@@ -1,15 +1,9 @@
 import moment from "jalali-moment";
-import {PersianMonths} from "./month.js";
 
 export const GenerateDate = (date) => {
     let newDate = moment(date);
     let year = newDate.jYear();
-
-    // when we want to show name of months
-    // let month = PersianMonths[newDate.jMonth()];
-
     let month = newDate.jMonth();
-    let day = newDate.jDate();
 
     let firstDateOfMonth = moment(`${year}-${month + 1}-01`, 'jYYYY-jMM-jDD').startOf('jMonth');
     let lastDateOfMonth = moment(`${year}-${month + 1}-${newDate.jDaysInMonth()}`, 'jYYYY-jMM-jDD').endOf('jMonth');
