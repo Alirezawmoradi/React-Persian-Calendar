5c972734392375b3f2ed20f1dd2acf62664e89f5
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-20 00:57:32 +0330

    calendar styles are creating

--- src/components/dropdown/calendar.jsx
+++ src/components/dropdown/calendar.jsx
@@ -1,11 +1,19 @@
-import {GenerateDate} from "../../utils/generate-date.js";
+import {generateDate} from "../../utils/generate-date.js";
+import moment from "jalali-moment";
 
 export const Calendar = () => {
-    console.log(GenerateDate())
     return (
-        <div className='flex flex-col justify-center w-screen h-auto px-7'>
-            <div className='rounded-lg border bg-gray-200'>
-                Show Calendar
+        <div className='flex flex-col w-96 h-96'>
+            <div className='rounded-lg border bg-gray-200 grid grid-cols-7 font-medium'>
+                {
+                    generateDate().map(({date, currentMonth, today}, index) => {
+                        return(
+                            <div key={index}>
+                                <h1>{moment(date, 'jYYYY-jM-jD').format('jD')}</h1>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
