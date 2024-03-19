4908b9b9e68a1044bbbe616da7d206c175ab7b45
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-20 01:13:04 +0330

    days added to calendar component

--- src/components/dropdown/calendar.jsx
+++ src/components/dropdown/calendar.jsx
@@ -1,14 +1,24 @@
 import {generateDate} from "../../utils/generate-date.js";
 import moment from "jalali-moment";
+import {PersianDays} from "../../utils/date.js";
 
 export const Calendar = () => {
     return (
         <div className='flex flex-col w-96 h-96'>
-            <div className='rounded-lg border bg-gray-200 grid grid-cols-7 font-medium'>
+            <div className='grid grid-cols-7 w-full'>
+                {
+                    PersianDays.map((day, index) => {
+                        return (
+                            <h1 className='h-14 grid place-content-center text-sm' key={index}>{day}</h1>
+                        )
+                    })
+                }
+            </div>
+            <div className='grid grid-cols-7 w-full font-medium'>
                 {
                     generateDate().map(({date, currentMonth, today}, index) => {
-                        return(
-                            <div key={index}>
+                        return (
+                            <div className='h-14 border grid place-content-center' key={index}>
                                 <h1>{moment(date, 'jYYYY-jM-jD').format('jD')}</h1>
                             </div>
                         )
