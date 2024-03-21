abaee15ef3a7de8e159e2d78a0024a25923874ad
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-21 17:09:46 +0330

    code refactored

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,30 +1,8 @@
-import {useState} from "react";
 import {Calendar} from "./calendar.jsx";
 
 export const Dropdown = () => {
-    const [showDatePicker, setShowDatePicker] = useState(false);
-
+    //it should change to input with icon to show calendar
     return (
-        // <div>
-        //     <button className='bg-gray-300 hover:bg-gray-400 outline-none w-28 h-9 rounded-full'
-        //             onClick={() => setShowDatePicker(!showDatePicker)}>
-        //         {
-        //             showDatePicker ?
-        //                 (
-        //                     <div className='flex flex-col py-1 items-center'>
-        //                         <div>
-        //                             <span>Close</span>
-        //                         </div>
-        //                         <div className='mt-3'>
-                                    <Calendar/>
-        //                         </div>
-        //                     </div>
-        //                 )
-        //                 :
-        //                 <span>Date Picker</span>
-        //         }
-        //     </button>
-        // </div>
-
+        <Calendar/>
     )
 }
