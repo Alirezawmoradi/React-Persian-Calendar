51948c1736dce4489bd47de2ea2f0e416dce6a50
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-21 17:08:28 +0330

    code refactored

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -5,26 +5,26 @@ export const Dropdown = () => {
     const [showDatePicker, setShowDatePicker] = useState(false);
 
     return (
-        <div>
-            <button className='bg-gray-300 hover:bg-gray-400 outline-none w-28 h-9 rounded-full'
-                    onClick={() => setShowDatePicker(!showDatePicker)}>
-                {
-                    showDatePicker ?
-                        (
-                            <div className='flex flex-col py-1 items-center'>
-                                <div>
-                                    <span>Close</span>
-                                </div>
-                                <div className='mt-3'>
+        // <div>
+        //     <button className='bg-gray-300 hover:bg-gray-400 outline-none w-28 h-9 rounded-full'
+        //             onClick={() => setShowDatePicker(!showDatePicker)}>
+        //         {
+        //             showDatePicker ?
+        //                 (
+        //                     <div className='flex flex-col py-1 items-center'>
+        //                         <div>
+        //                             <span>Close</span>
+        //                         </div>
+        //                         <div className='mt-3'>
                                     <Calendar/>
-                                </div>
-                            </div>
-                        )
-                        :
-                        <span>Date Picker</span>
-                }
-            </button>
-        </div>
+        //                         </div>
+        //                     </div>
+        //                 )
+        //                 :
+        //                 <span>Date Picker</span>
+        //         }
+        //     </button>
+        // </div>
 
     )
 }
