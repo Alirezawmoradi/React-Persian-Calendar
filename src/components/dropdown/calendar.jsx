d063262f80a16770e11bcf1229133e03eee8c4e0
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-21 15:57:25 +0330

    change style in select date to show selected date added

--- src/components/dropdown/calendar.jsx
+++ src/components/dropdown/calendar.jsx
@@ -7,6 +7,7 @@ import {GrFormNext, GrFormPrevious} from "react-icons/gr";
 export const Calendar = () => {
     const currentDate = moment();
     const [today, setToday] = useState(currentDate);
+    const [selectedDate, setSelectedDate] = useState(currentDate);
     return (
         <div className='flex mx-auto divide-x-2 items-center gap-10 h-screen'>
             <div className='flex flex-col w-96 h-96'>
@@ -17,7 +18,7 @@ export const Calendar = () => {
                                         onClick={() => {
                                             setToday(moment(today).jMonth(today.jMonth() - 1))
                                         }}/>
-                        <h1 className='cursor-pointer' onClick={()=>setToday(currentDate)}>Today</h1>
+                        <h1 className='cursor-pointer' onClick={() => setToday(currentDate)}>Today</h1>
                         <GrFormNext className='w-5 h-5 cursor-pointer'
                                     onClick={() => {
                                         setToday(moment(today).jMonth(today.jMonth() + 1))
@@ -36,10 +37,16 @@ export const Calendar = () => {
                 </div>
                 <div className='grid grid-cols-7 w-full font-medium text-sm'>
                     {
-                        generateDate(moment(today).jMonth(today.jMonth()), moment(today).jYear(today.jYear())).map(({date, currentMonth, today}, index) => {
+                        generateDate(moment(today).jMonth(today.jMonth()), moment(today).jYear(today.jYear())).map(({
+                                                                                                                        date,
+                                                                                                                        currentMonth,
+                                                                                                                        today
+                                                                                                                    }, index) => {
                             return (
                                 <div className='h-14 border grid place-content-center' key={index}>
-                                    <h1 className={`${currentMonth ? '' : 'text-gray-400'} ${today ? 'bg-red-600 text-white' : ''} h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all duration-300 cursor-pointer`}>
+                                    <h1 className={`${currentMonth ? '' : 'text-gray-400'} ${today ? 'bg-red-600 text-white' : ''} ${moment(selectedDate.toDate().toDateString()).format('jYYYY/jMM/jDD') === moment(date, 'jYYYY-jM-jD').format('jYYYY/jMM/jDD') ? "bg-black text-white" : ""} h-10 w-10 grid place-content-center rounded-full hover:bg-black hover:text-white transition-all duration-300 cursor-pointer`}
+                                        onClick={() => setSelectedDate(moment(date, 'jYYYY-jM-jD'))}
+                                    >
                                         {moment(date, 'jYYYY-jM-jD').format('jD')}
                                     </h1>
                                 </div>
@@ -49,7 +56,7 @@ export const Calendar = () => {
                 </div>
             </div>
             <div className='h-96 w-52 px-5'>
-                <h1 className='font-semibold'>{moment(today.toDate().toDateString()).format('jYYYY/jMM/jDD')}</h1>
+                <h1 className='font-semibold'>{moment(selectedDate.toDate().toDateString()).format('jYYYY/jMM/jDD')}</h1>
             </div>
         </div>
     )
