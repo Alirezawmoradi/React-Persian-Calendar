3ea7e13ad92a54993704ceb137456e09d15cdb62
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-24 15:05:55 +0330

    state for showing calendar added

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,15 +1,22 @@
 import {Calendar} from "./calendar.jsx";
 import {VscCalendar} from "react-icons/vsc";
+import {useState} from "react";
 
 export const Dropdown = () => {
     //it should change to input with icon to show calendar
+    const [showCalendar, setShowCalendar] = useState(false);
     return (
         <div className='flex flex-col justify-center'>
-            <Calendar/>
-            <div className='flex justify-center gap-5 mb-16'>
+            <div className='flex justify-center gap-5 mt-20'>
                 <input className='border outline-none rounded-full h-10 bg-gray-100 hover:bg-gray-200 pl-3'/>
-                <VscCalendar className='h-10 w-5 cursor-pointer'/>
+                <VscCalendar className='h-10 w-5 cursor-pointer' onClick={() => setShowCalendar(!showCalendar)}/>
             </div>
+            {
+                showCalendar &&
+                <div className='mb-5'>
+                    <Calendar/>
+                </div>
+            }
         </div>
     )
 }
