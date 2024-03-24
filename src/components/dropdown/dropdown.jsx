b9556fb1d43b12dcc5262c6fd84e8062b7df20a0
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-24 15:34:01 +0330

    state for showing date in input created

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,14 +1,18 @@
 import {Calendar} from "./calendar.jsx";
 import {VscCalendar} from "react-icons/vsc";
 import {useState} from "react";
+import moment from "jalali-moment";
 
 export const Dropdown = () => {
     //it should change to input with icon to show calendar
     const [showCalendar, setShowCalendar] = useState(false);
+    const [date, setDate] = useState(moment().format('jYYYY/jMM/jDD'));
     return (
         <div className='flex flex-col justify-center'>
             <div className='flex justify-center gap-5 mt-20'>
-                <input className='border outline-none rounded-full h-10 bg-gray-100 hover:bg-gray-200 pl-3'/>
+                <input className='border outline-none rounded-full h-10 bg-gray-100 hover:bg-gray-200 pl-3'
+                       value={date}
+                       onChange={() => setDate(date)}/>
                 <VscCalendar className='h-10 w-5 cursor-pointer' onClick={() => setShowCalendar(!showCalendar)}/>
             </div>
             {
