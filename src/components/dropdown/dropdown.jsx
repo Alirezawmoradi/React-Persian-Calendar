b20228b3f1d13952b56ff1aaf457ee4d806107ef
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-26 14:17:40 +0330

    input value change to selected date using app context

--- src/components/dropdown/dropdown.jsx
+++ src/components/dropdown/dropdown.jsx
@@ -1,27 +1,24 @@
 import {Calendar} from "./calendar.jsx";
 import {VscCalendar} from "react-icons/vsc";
 import {useState} from "react";
-import moment from "jalali-moment";
+import {useAppContext} from "../../contexts/app/app-context.jsx";
 
 export const Dropdown = () => {
     //it should change to input with icon to show calendar
     const [showCalendar, setShowCalendar] = useState(false);
-    const [date, setDate] = useState(moment().format('jYYYY/jMM/jDD'));
-    const inputFill = (event) => {
-        console.log(event.target)
-    }
+    const {selectedDate, changeSelectedDate} = useAppContext();
     return (
         <div className='flex flex-col justify-center'>
             <div className='flex justify-center gap-5 mt-20'>
                 <input className='border outline-none rounded-full h-10 bg-gray-100 hover:bg-gray-200 pl-3'
-                       value={date}
-                       onChange={() => setDate(date)}/>
+                       value={selectedDate.format('jYYYY/jMM/jDD')}
+                       onChange={() => changeSelectedDate(selectedDate)}/>
                 <VscCalendar className='h-10 w-5 cursor-pointer' onClick={() => setShowCalendar(!showCalendar)}/>
             </div>
             {
                 showCalendar &&
                 <div className='mb-5'>
-                    <Calendar inputFill={inputFill}/>
+                    <Calendar/>
                 </div>
             }
         </div>
