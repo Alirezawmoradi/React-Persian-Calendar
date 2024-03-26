c9f2267b10e66a65210b4f545d26c356f0daacb4
Alirezaw <Alirezawmoradi@gmail.com>
2024-03-26 14:09:21 +0330

    app provider added to project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {AppProvider} from "./contexts/app/app-context.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppProvider>
+        <App/>
+    </AppProvider>)
