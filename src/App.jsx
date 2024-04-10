a98b92567ddee4fedafc9619442854c40e2ca19f
Alirezaw <Alirezawmoradi@gmail.com>
2024-04-10 23:10:55 +0330

    code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Calendar} from "./feature/calendar/calendar.jsx";
 function App() {
 
     return (
-        <div className='flex flex-col justify-center'>
+        <div className='flex justify-center'>
             <Calendar/>
         </div>
     )
