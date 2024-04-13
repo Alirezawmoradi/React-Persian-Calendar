6e965fc8f2410ae08ae1b03503d1b64252c8e481
Alirezaw <Alirezawmoradi@gmail.com>
2024-04-13 19:24:08 +0330

    today onClick changed to close all open dropdowns

--- src/feature/calendar/calendar.jsx
+++ src/feature/calendar/calendar.jsx
@@ -30,6 +30,8 @@ export const Calendar = () => {
                         onClick={() => {
                             changeSelectedDate(currentDate)
                             changeToday(currentDate)
+                            setShowYearDropdown(false)
+                            setShowMonthDropdown(false)
                         }}>امروز</h1>
                     <div className='flex items-center gap-5'>
                         <GrFormPrevious className='w-5 h-5 cursor-pointer'
