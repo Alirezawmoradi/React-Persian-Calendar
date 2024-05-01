720bd785e060334ab440ac188988f1a6ed420f78
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-01 20:04:22 +0330

    change theme added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import {Calendar} from "./feature/calendar/calendar.jsx";
+import ChangeTheme from "./components/change-theme/change-theme.jsx";
+import {useAppContext} from "./contexts/app/app-context.jsx";
 
 function App() {
-
+    const {theme} = useAppContext();
     return (
-        <div className='flex justify-center'>
-            <Calendar/>
+        <div className={`${theme}`}>
+            <div className='flex justify-center bg-light dark:bg-dark transition-colors duration-500'>
+                <nav className='flex justify-start gap-2 align-middle py-4'>
+                    <ChangeTheme/>
+                </nav>
+                <Calendar/>
+            </div>
         </div>
     )
 }
