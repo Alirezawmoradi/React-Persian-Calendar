6fa1f7de71c589e6c04eb9b67ba39a92c217e20f
Alirezaw <Alirezawmoradi@gmail.com>
2024-05-01 20:05:27 +0330

    dark theme added to year dropdown

--- src/components/dropdown/year-dropdown/year-dropdown.jsx
+++ src/components/dropdown/year-dropdown/year-dropdown.jsx
@@ -7,7 +7,7 @@ import {PersianNumber} from "../../../utils/persian-number.js";
 export const YearDropdown = () => {
     const startYear = 1300;
     const currentYear = moment().jYear();
-    const {selectedDate, changeSelectedDate,changeToday,today} = useAppContext();
+    const {selectedDate, changeSelectedDate, changeToday, today} = useAppContext();
 
     const years = [];
     for (let year = startYear; year <= currentYear; year++) {
@@ -43,8 +43,8 @@ export const YearDropdown = () => {
         }
     }
     const handleCurrentYear = () => {
-        changeSelectedDate(moment(currentYear,'jYYYY'));
-        changeToday(moment(currentYear,'jYYYY'))
+        changeSelectedDate(moment(currentYear, 'jYYYY'));
+        changeToday(moment(currentYear, 'jYYYY'))
     }
     const handleNextYear = () => {
         const currentYear = parseInt(selectedDate.format('jYYYY'));
@@ -59,32 +59,34 @@ export const YearDropdown = () => {
         <div className='flex flex-col w-96 h-96'>
             <div className='flex flex-row-reverse justify-center'>
                 <div className='flex justify-center items-center gap-5 mb-14'>
-                    <GrFormPrevious className='w-5 h-5 cursor-pointer'
-                                    onClick={handlePrevYear}/>
-                    <h1 className='flex items-center justify-center cursor-pointer border w-20 rounded-full bg-blue-700 hover:bg-blue-800 h-8 text-white transition-all duration-300 text-sm'
+                    <GrFormPrevious
+                        className='w-5 h-5 cursor-pointer dark:text-gray-300 dark:hover:text-white hover:text-gray-500 transition-all duration-300'
+                        onClick={handlePrevYear}/>
+                    <h1 className='flex items-center justify-center cursor-pointer border w-20 rounded-full bg-blue-700 dark:bg-blue-800 dark:border-gray-700 dark:hover:bg-blue-600 hover:bg-blue-800 h-8 text-white transition-all duration-300 text-sm'
                         onClick={handleCurrentYear}
                     >
                         سال جاری
                     </h1>
-                    <GrFormNext className='w-5 h-5 cursor-pointer'
-                                onClick={handleNextYear}/>
+                    <GrFormNext
+                        className='w-5 h-5 cursor-pointer dark:text-gray-300 dark:hover:text-white hover:text-gray-500 transition-all duration-300'
+                        onClick={handleNextYear}/>
                 </div>
             </div>
-            <div className='grid grid-cols-4 w-full font-medium text-sm'>
+            <div className='grid grid-cols-4 dark:divide-gray-600 w-full font-medium text-sm dark:text-gray-200'>
                 {
                     years.reverse().slice(startIndex, endIndex).map((year, index) => {
                         const isSelected = selectedDate.format('jYYYY/jMM/jDD') === moment(year, 'jYYYY-jM-jD').format('jYYYY/jMM/jDD');
                         return (
                             <div
-                                className='h-14 border grid place-content-center cursor-pointer hover:bg-gray-200 hover:rounded-md transition-all duration-300'
+                                className='h-14 border dark:border-gray-600 grid place-content-center cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 hover:rounded-md transition-all duration-300'
                                 key={index}
                                 onClick={() => {
                                     changeSelectedDate(moment(year, 'jYYYY'))
-                                    changeToday(moment(year,'jYYYY'))
+                                    changeToday(moment(year, 'jYYYY'))
                                 }}
                             >
                                 <h1 className={`h-10 w-10 grid place-content-center
-                                ${isSelected ? 'bg-red-600 text-white h-14 w-24' : ''}
+                                ${isSelected ? 'bg-red-600 dark:bg-blue-600 text-white h-14 w-24' : ''}
                                 rounded-md transition-all duration-300`}
                                 >{PersianNumber(year)}</h1>
                             </div>
@@ -93,11 +95,15 @@ export const YearDropdown = () => {
                 }
             </div>
             <div className='flex justify-center items-center gap-5 mt-5'>
-                <GrFormPrevious className='w-5 h-5 cursor-pointer' onClick={handlePrevPage}/>
-                <h1 className='flex items-center justify-center cursor-pointer border w-32 rounded-full bg-blue-700 hover:bg-blue-800 h-8 text-white transition-all duration-300 text-sm'>
+                <GrFormPrevious
+                    className='w-5 h-5 cursor-pointer dark:text-gray-300 dark:hover:text-white hover:text-gray-500 transition-all duration-300'
+                    onClick={handlePrevPage}/>
+                <h1 className='flex items-center justify-center cursor-pointer border w-32 rounded-full bg-blue-700 dark:border-gray-700 dark:hover:text-white dark:border-none hover:bg-blue-800 h-8 text-white transition-all duration-300 text-sm'>
                     صفحه {PersianNumber(currentPage)} از {PersianNumber(totalPages)}
                 </h1>
-                <GrFormNext className='w-5 h-5 cursor-pointer' onClick={handleNextPage}/>
+                <GrFormNext
+                    className='w-5 h-5 cursor-pointer dark:text-gray-300 dark:hover:text-white hover:text-gray-500 transition-all duration-300'
+                    onClick={handleNextPage}/>
             </div>
         </div>
     )
